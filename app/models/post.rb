b204c365a66ae748ede0afc15ac377c11b761f0e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-18 00:43:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(categories_attributes)
-  	categories_attributes.each do |i, category_attr|  
-	  	if category_attr[:name].present?		
-	  		category = Category.find_or_create_by(name: category_attr[:name])
-	  		if !self.categories.include?(category)
-	  			self.post_categories.build(:category => category)
-	  		end
-	  	end
-  	end
-  end
+#  def categories_attributes=(categories_attributes)
+#  	categories_attributes.each do |i, category_attr|  
+#	  	if category_attr[:name].present?		
+#	  		category = Category.find_or_create_by(name: category_attr[:name])
+#	  		if !self.categories.include?(category)
+#	  			self.post_categories.build(:category => category)
+#	  		end
+#	  	end
+# 	end
+#  end
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end
